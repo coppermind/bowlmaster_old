6531dbcbe512b1168bad06c9bd7dabb57ed3671e
Paul Corpuz <paul.corpuz@gmail.com>
2015-04-25 15:48:28 +0800

    Initial code commit.
    * Game scene done.
    * Lane done.
    * Pins laid out.
    * Ball basic behavior added.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public Vector3 velocity;
+
+	AudioSource audioSource;
+	Rigidbody rigidBody;
+
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+		audioSource.Play();
+		
+		rigidBody = GetComponent<Rigidbody>();
+		rigidBody.velocity = velocity;
+	}
+
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public Ball ball;
+	
+	private float distance;
+	
+	void Start () {
+		ball = FindObjectOfType<Ball>();
+		distance = transform.position.z - ball.transform.position.z;
+	}
+	
+	void Update () {
+		if (transform.position.z < 1700) {
+			float newDistance = ball.transform.position.z + distance;
+			transform.position = new Vector3(transform.position.x, transform.position.y, newDistance);
+		}
+	}
+}
