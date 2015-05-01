c685ac19099ea8acb052e65b154998897eb122e6
Paul Corpuz <paul.corpuz@gmail.com>
2015-05-02 00:24:35 +0800

    Add pin swiper function

---
+++ Assets/Scripts/PinsFormation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinsFormation : MonoBehaviour {
+
+	void Update () {
+		if (0 >= transform.childCount) {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Swiper.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swiper : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject pinsFormation;
+	
+	private bool droppingPins = false;
+
+	void Update () {
+	
+	}
+	
+	private void ResetPins() {
+		GameObject formation = Instantiate(pinsFormation, new Vector3(0, 45f, 1829f), Quaternion.identity) as GameObject;
+		foreach (Transform pinTransform in formation.transform) {
+			Rigidbody pinRigidBody = pinTransform.gameObject.GetComponent<Rigidbody>();
+			pinRigidBody.isKinematic = true;
+		}
+	}
+	
+	private void DropPins() {
+		PinsFormation formation = FindObjectOfType<PinsFormation>();
+		foreach (Transform pinTransform in formation.transform) {
+			Rigidbody pinRigidBody = pinTransform.gameObject.GetComponent<Rigidbody>();
+			pinRigidBody.isKinematic = false;
+		}
+	}
+	
+	private void RaisePins() {
+		PinsFormation formation = FindObjectOfType<PinsFormation>();
+		foreach (Transform pinTransform in formation.transform) {
+			Rigidbody pinRigidBody = pinTransform.gameObject.GetComponent<Rigidbody>();
+			pinRigidBody.isKinematic = true;
+		}
+	}
+}
