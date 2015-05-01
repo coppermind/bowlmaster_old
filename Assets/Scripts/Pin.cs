a82928931df18d613966888db58c9ee2fee5b472
Paul Corpuz <paul.corpuz@gmail.com>
2015-05-02 00:24:58 +0800

    Add pin setter and pin behavior

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pin : MonoBehaviour {
+
+	[SerializeField]
+	private float yEventHorizon = -20f;
+	
+	[SerializeField]
+	private float standingThreshold = 11.3f;
+	
+	void Update () {
+		if (yEventHorizon > transform.position.y) {
+			Destroy(gameObject);
+		}
+	}
+	
+	public bool IsStanding() {
+		Vector3 eulerRotation = transform.rotation.eulerAngles;
+		
+		float xTilt = Mathf.Abs(eulerRotation.x);
+		float zTilt = Mathf.Abs(eulerRotation.y);
+
+		float reverseThreshold = 360 - standingThreshold;
+		if (xTilt > standingThreshold && xTilt < reverseThreshold) {
+			return false;
+		}
+		if (zTilt > standingThreshold && zTilt < reverseThreshold) {
+			return false;
+		}
+		return true;
+
+	}
+}
---
+++ Assets/Scripts/PinSetter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PinSetter : MonoBehaviour {
+
+	Text totalText;
+	
+	bool ballEnteredBox = false;
+
+	void Update () {
+		GameObject pinTotalObject = GameObject.Find("Pin Total");
+		totalText = pinTotalObject.GetComponent<Text>();
+		totalText.text = CountStanding().ToString();
+	}
+	
+	void OnTriggerEnter(Collider collider) {
+		Ball ball = collider.GetComponent<Ball>();
+		if (ball) {
+			ballEnteredBox = true;
+			totalText.color = new Color(1f, 0.25f, 0.25f);
+		}
+	}
+	
+	void OnTriggerExit(Collider collider) {
+		if (collider.name == "Pin_Collider") {
+			Destroy(collider.gameObject);
+		}
+	}
+	
+	public int CountStanding() {
+		int standing = 0;
+		foreach (Pin pin in FindObjectsOfType<Pin>()) {
+			if (pin.IsStanding()) { standing++; }
+		}
+		return standing;
+	}
+}
