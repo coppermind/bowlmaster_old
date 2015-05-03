f0b0ca7352c3eb36ffe422ae8e98416dcef219a4
Paul Corpuz <paul.corpuz@gmail.com>
2015-05-03 21:34:55 +0800

    Included Unity Test Tools

---
+++ Assets/Scripts/ScoreMaster.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreMaster : MonoBehaviour {
+
+	public enum Action {Reset, Tidy};
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+	
+
+}
