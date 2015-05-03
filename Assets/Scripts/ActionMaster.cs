bf0f839c3fea18baa4d3a15364a175d783fd42e3
Paul Corpuz <paul.corpuz@gmail.com>
2015-05-03 22:53:01 +0800

    Started adding in some tests.

---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionMaster {
+
+	public enum Action { Tidy, Reset, EndTurn, EndGame }
+	
+	private int[] bowls = new int[21];
+	private int bowl = 1;
+	
+	public Action Bowl(int pins) {
+		if (pins < 0 || pins > 10) { throw new UnityException("Invalid pin count detected."); }
+	
+		// Strike!
+		if (pins == 10) { return Action.EndTurn; }
+		
+		if (bowl % 2 != 0) {
+			bowl++;
+			return Action.Tidy; 
+		} else {
+			bowl = 1;
+			return Action.EndTurn;
+		}
+		
+		throw new UnityException("Not sure what action to return! smh");
+	}
+}
---
+++ Assets/Spec/Editor/ActionMasterTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class ActionMasterTest {
+
+	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+	private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+	private ActionMaster.Action reset = ActionMaster.Action.Reset;
+	
+	private ActionMaster actionMaster;
+	
+	[SetUp]
+	public void Setup() {
+		actionMaster = new ActionMaster();
+	}
+	
+	[Test]
+	public void FirstTossStrikeReturnsEndTurn() {
+		Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+	}
+	
+	[Test]
+	public void FirstTossLessThan10ReturnsTidy() {
+		Assert.AreEqual(tidy, actionMaster.Bowl(9));
+	}
+	
+	[Test]
+	public void SecondTossReturnsEndTurn() {
+		actionMaster.Bowl(1);
+		Assert.AreEqual(endTurn, actionMaster.Bowl(1));
+	}
+	
+}
